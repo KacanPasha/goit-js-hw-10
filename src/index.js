dbe3d5d45a3752256bc9f1a0b0ca44bd2d2f1207
KacanPasha <129742576+KacanPasha@users.noreply.github.com>
2023-09-10 14:55:07 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -14,25 +14,23 @@ elements.select.addEventListener('change', onChangeSelect);
 getBreeds();
 
 function onChangeSelect(evt) {
-  elements.catInfo.classList.add('is-hidden');
+  elements.catInfo.classList.remove('is-visible');
   elements.loader.classList.remove('is-hidden');
   const breedId = evt.currentTarget.value;
   fetchCatByBreed(breedId)
     .then(breed => {
       elements.catInfo.innerHTML = createMarkupCat(breed);
       elements.loader.classList.add('is-hidden');
-      elements.catInfo.classList.remove('is-hidden');
+      elements.catInfo.classList.add('is-visible');
     })
     .catch(handleError);
 }
 
 function getBreeds() {
-  elements.select.classList.add('is-hidden');
-  elements.error.classList.add('is-hidden');
   fetchBreeds()
     .then(breeds => {
       elements.select.insertAdjacentHTML("beforeend", createSelectBreeds(breeds));
-      elements.select.classList.remove('is-hidden');
+      elements.select.classList.add('is-visible');
       elements.loader.classList.add('is-hidden');
       new SlimSelect({
         select: '.breed-select',
@@ -48,6 +46,8 @@ function createSelectBreeds(breeds) {
 }
 
 function createMarkupCat(breed) {
+  if (breed.length === 0) return null;
+
   const [{ url, breeds: [{ name, description, temperament }] }] = breed;
   return `<img class="cat-img" src="${url}" alt="" width="600"/>
     <div class="wrap-descr">
@@ -63,6 +63,7 @@ function handleError(error) {
   console.error("Ошибка:", error);
   showErrorMessage();
   elements.loader.classList.add('is-hidden');
+  elements.select.classList.add('is-hidden');
 }
 
 function showErrorMessage() {
